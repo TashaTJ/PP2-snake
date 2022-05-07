99441d31b01efe836a8792a28c5c3ccb1abf6b7b
Tasha Johnson <tashataylorjohnson@me.com>
2022-05-07 20:08:12 +0000

    Add start game function to the script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,52 @@
-
 // Game Screen variables 
 const gameScreen = document.getElementById('game-screen');
 
 // Game variables 
 const grid = document.querySelector(".grid");
- /* const startButton = document.getElementById("start"); */
- const scoreDisplay = document.getElementById("score");
- let squares = [];
- let currentSnake = [2, 1, 0];
- let direction = 1;
- const width = 10;
- let appleIndex = 0;
- let score = 0;
- let intervalTime = 1000;
- let speed = 0.8;
- let timerId = 0;
- let gameOver = false;
+/* const startButton = document.getElementById("start"); */
+const scoreDisplay = document.getElementById("score");
+let squares = [];
+let currentSnake = [2, 1, 0];
+let direction = 1;
+const width = 10;
+let appleIndex = 0;
+let score = 0;
+let intervalTime = 1000;
+let speed = 0.8;
+let timerId = 0;
+let gameOver = false;
 
 function createGrid() {
     //create 100 of these elements with a for loop
     for (let i = 0; i < width * width; i++) {
-      //create element
-      const square = document.createElement("div");
-      //add styling to the element
-      square.classList.add("square");
-      //put the element into our grid
-      grid.append(square);
-      //push it into a new squares array
-      squares.push(square);
+        //create element
+        const square = document.createElement("div");
+        //add styling to the element
+        square.classList.add("square");
+        //put the element into our grid
+        grid.append(square);
+        //push it into a new squares array
+        squares.push(square);
     }
-  }
-  createGrid();
+}
+createGrid();
+
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
+
+function startGame() {
+    //remove the snake
+    currentSnake.forEach((index) => squares[index].classList.remove("snake"));
+    //remove the apple
+    squares[appleIndex].classList.remove("apple");
+    clearInterval(timerId);
+    currentSnake = [2, 1, 0];
+    score = 0;
+    //re add new score to browser
+    scoreDisplay.textContent = score;
+    direction = 1;
+    intervalTime = 1000;
+    generateApple();
+    //readd the class of snake to our new currentSnake
+    currentSnake.forEach((index) => squares[index].classList.add("snake"));
+    timerId = setInterval(move, intervalTime);
+}
